5cda390de8154dac680a5943121a7d863441a622
Raphael Pedroso <raphael.pedroso97@hotmail.com>
2024-07-08 17:15:36 -0300

    Service layer, component registration

--- src/main/java/com/personal/course/repositories/UserRepository.java
+++ src/main/java/com/personal/course/repositories/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.personal.course.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
-	
-}
+public interface UserRepository extends JpaRepository<User, Long> {
 
+}
